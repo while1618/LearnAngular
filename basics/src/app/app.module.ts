4cf908c01747446a5bf9f26013dbdb9036e13583
Dejan Zdravkovic <bagzi1996@gmail.com>
2020-11-25 01:39:53 +0100

    assignment 1

--- basics/src/app/app.module.ts
+++ basics/src/app/app.module.ts
@@ -4,9 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessComponent } from './success/success.component';
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessComponent,
+    WarningComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ basics/src/app/warning/warning.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'warning',
+  template: `<h3 class="warning">Warning</h3>`,
+  styles: ['.warning { color: orange; }'],
+})
+export class WarningComponent {}

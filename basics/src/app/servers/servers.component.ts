d65e5801d057007dd9143165b7396cc6a2b0e1d1
Dejan Zdravkovic <bagzi1996@gmail.com>
2020-11-27 17:11:29 +0100

    dark theme and warning fix

--- basics/src/app/servers/servers.component.ts
+++ basics/src/app/servers/servers.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class ServersComponent {}
--- basics/src/app/warning/warning.component.ts
+++ basics/src/app/warning/warning.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'warning',
   template: `<h3 class="warning">Warning</h3>`,
-  styles: ['.warning { color: orange; }'],
+  styles: ['.warning { color: #ffc107; }'],
 })
 export class WarningComponent {}

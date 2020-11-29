0358ca7c31c0fc79a70afe1672715edcb82b262c
Dejan Zdravkovic <bagzi1996@gmail.com>
2020-11-29 19:38:26 +0100

    Section 1: Directives

--- basics/src/app/server/server.component.ts
+++ basics/src/app/server/server.component.ts
@@ -3,12 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        font-size: large;
+      }
+    `,
+  ],
 })
 export class ServerComponent {
   public serverId: number = 10;
-  private serverStatus: string = 'offline';
+  public serverStatus: string;
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   public getServerStatus() {
     return this.serverStatus;
+  }
+
+  public getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
--- basics/src/app/servers/servers.component.ts
+++ basics/src/app/servers/servers.component.ts
@@ -4,12 +4,23 @@ import { Component } from '@angular/core';
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
+  styles: [
+    `
+      .log {
+        font-size: large;
+      }
+    `,
+  ],
 })
 export class ServersComponent {
   public allowNewServer = false;
   public serverCreationStatus = 'No server was created';
   public serverName = 'Default Server Name';
   public username = '';
+  public serverCreated = false;
+  public servers = ['TestServer', 'ProdServer'];
+  public showDetails = false;
+  public log = [];
 
   public constructor() {
     setTimeout(() => {
@@ -19,9 +30,16 @@ export class ServersComponent {
 
   public onCreateServer() {
     this.serverCreationStatus = `Server was created. Name is ${this.serverName}`;
+    this.servers.push(this.serverName);
+    this.serverCreated = true;
   }
 
   public onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  public toggle() {
+    this.showDetails = !this.showDetails;
+    this.log.push(new Date());
   }
 }

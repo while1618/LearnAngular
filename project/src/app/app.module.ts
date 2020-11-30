bba9ac6f5b3d22c22aeb6337cdde84a4412d050d
Dejan Zdravkovic <bagzi1996@gmail.com>
2020-11-30 15:36:47 +0100

    Rename project and finish basics project section

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
 import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.component';
 import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
 import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
 import { RecipeComponent } from './recipe/recipe.component';
 
@@ -18,7 +18,7 @@ import { RecipeComponent } from './recipe/recipe.component';
     RecipeItemComponent,
     RecipeDetailsComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent,
+    ShoppingEditComponent,
     RecipeComponent,
   ],
   imports: [BrowserModule],

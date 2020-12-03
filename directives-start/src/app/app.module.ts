f90dadeede9d84bc588014ef4a66ba8dcf8dd017
Dejan Zdravkovic <bagzi1996@gmail.com>
2020-12-03 14:46:02 +0100

    Section 7 - Directives

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { BasicHighlightDirective } from "./highligh/basic-highlight.directive";
+import { BetterHighlightDirective } from "./highligh/better-highlight.directive";
+import { UnlessDirective } from "./unless/unless.directive";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective,
+    UnlessDirective,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

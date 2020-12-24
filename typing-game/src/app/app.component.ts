d3e83ddd41cb5135510e35c15ff903f5c09ae24e
Dejan Zdravkovic <bagzi1996@gmail.com>
2020-12-24 19:13:37 +0100

    Section 4: typing game complete

--- typing-game/src/app/app.component.ts
+++ typing-game/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing-game';
+  generatedText = lorem.sentence();
+  userInput = '';
+
+  onInputChange(value: string) {
+    this.userInput = value;
+  }
+
+  pickLetterColor(generatedLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return generatedLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }

e2d2319d85e6cd6c727662ea22918f36e29ccee7
Dejan Zdravkovic <bagzi1996@gmail.com>
2020-12-24 18:27:04 +0100

    Section 3: cards complete

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imgUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Snowy Mountain',
+      imgUrl: 'assets/mountain.jpeg',
+      username: 'mountain_lover',
+      content: 'Here is a picture of a snowy mountain',
+    },
+    {
+      title: 'Mountain Biking',
+      imgUrl: 'assets/biking.jpeg',
+      username: 'biking123',
+      content: 'I did some biking today',
+    },
+  ];
 }

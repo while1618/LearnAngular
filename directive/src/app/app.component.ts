eaec0fca65914495034e6466cd42f040dff3d272
Dejan Zdravkovic <bagzi1996@gmail.com>
2020-12-29 17:23:25 +0100

    Section 6: directives complete

--- directive/src/app/app.component.ts
+++ directive/src/app/app.component.ts
@@ -3,8 +3,144 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'directive';
+  currentPage = 0;
+  images = [
+    {
+      title: 'Baby Yoda',
+      url: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg',
+    },
+    {
+      title: 'Darth Vader',
+      url:
+        'https://www.denofgeek.com/wp-content/uploads/2017/03/darth-vader-1_0.jpg?fit=1947%2C1274',
+    },
+    {
+      title: 'Darth Maul',
+      url: 'https://upload.wikimedia.org/wikipedia/en/b/bf/Darth_Maul.png',
+    },
+    {
+      title: 'Baby Yoda',
+      url: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg',
+    },
+    {
+      title: 'Darth Vader',
+      url:
+        'https://www.denofgeek.com/wp-content/uploads/2017/03/darth-vader-1_0.jpg?fit=1947%2C1274',
+    },
+    {
+      title: 'Darth Maul',
+      url: 'https://upload.wikimedia.org/wikipedia/en/b/bf/Darth_Maul.png',
+    },
+    {
+      title: 'Baby Yoda',
+      url: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg',
+    },
+    {
+      title: 'Darth Vader',
+      url:
+        'https://www.denofgeek.com/wp-content/uploads/2017/03/darth-vader-1_0.jpg?fit=1947%2C1274',
+    },
+    {
+      title: 'Darth Maul',
+      url: 'https://upload.wikimedia.org/wikipedia/en/b/bf/Darth_Maul.png',
+    },
+    {
+      title: 'Baby Yoda',
+      url: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg',
+    },
+    {
+      title: 'Darth Vader',
+      url:
+        'https://www.denofgeek.com/wp-content/uploads/2017/03/darth-vader-1_0.jpg?fit=1947%2C1274',
+    },
+    {
+      title: 'Darth Maul',
+      url: 'https://upload.wikimedia.org/wikipedia/en/b/bf/Darth_Maul.png',
+    },
+    {
+      title: 'Baby Yoda',
+      url: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg',
+    },
+    {
+      title: 'Darth Vader',
+      url:
+        'https://www.denofgeek.com/wp-content/uploads/2017/03/darth-vader-1_0.jpg?fit=1947%2C1274',
+    },
+    {
+      title: 'Darth Maul',
+      url: 'https://upload.wikimedia.org/wikipedia/en/b/bf/Darth_Maul.png',
+    },
+    {
+      title: 'Baby Yoda',
+      url: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg',
+    },
+    {
+      title: 'Darth Vader',
+      url:
+        'https://www.denofgeek.com/wp-content/uploads/2017/03/darth-vader-1_0.jpg?fit=1947%2C1274',
+    },
+    {
+      title: 'Darth Maul',
+      url: 'https://upload.wikimedia.org/wikipedia/en/b/bf/Darth_Maul.png',
+    },
+    {
+      title: 'Baby Yoda',
+      url: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg',
+    },
+    {
+      title: 'Darth Vader',
+      url:
+        'https://www.denofgeek.com/wp-content/uploads/2017/03/darth-vader-1_0.jpg?fit=1947%2C1274',
+    },
+    {
+      title: 'Darth Maul',
+      url: 'https://upload.wikimedia.org/wikipedia/en/b/bf/Darth_Maul.png',
+    },
+    {
+      title: 'Baby Yoda',
+      url: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg',
+    },
+    {
+      title: 'Darth Vader',
+      url:
+        'https://www.denofgeek.com/wp-content/uploads/2017/03/darth-vader-1_0.jpg?fit=1947%2C1274',
+    },
+    {
+      title: 'Darth Maul',
+      url: 'https://upload.wikimedia.org/wikipedia/en/b/bf/Darth_Maul.png',
+    },
+    {
+      title: 'Baby Yoda',
+      url: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg',
+    },
+    {
+      title: 'Darth Vader',
+      url:
+        'https://www.denofgeek.com/wp-content/uploads/2017/03/darth-vader-1_0.jpg?fit=1947%2C1274',
+    },
+    {
+      title: 'Darth Maul',
+      url: 'https://upload.wikimedia.org/wikipedia/en/b/bf/Darth_Maul.png',
+    },
+    {
+      title: 'Baby Yoda',
+      url: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg',
+    },
+    {
+      title: 'Darth Vader',
+      url:
+        'https://www.denofgeek.com/wp-content/uploads/2017/03/darth-vader-1_0.jpg?fit=1947%2C1274',
+    },
+    {
+      title: 'Darth Maul',
+      url: 'https://upload.wikimedia.org/wikipedia/en/b/bf/Darth_Maul.png',
+    },
+  ];
+
+  checkPageIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 3;
+  }
 }

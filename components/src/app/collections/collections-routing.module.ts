96855e4ef4193a680c19d0f6ee161166918bf6eb
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-01-06 15:09:36 +0100

    tabs

--- components/src/app/collections/collections-routing.module.ts
+++ components/src/app/collections/collections-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BiographyComponent } from './biography/biography.component';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
-const routes: Routes = [{ path: '', component: CollectionsHomeComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: CollectionsHomeComponent,
+    children: [
+      { path: '', component: BiographyComponent },
+      { path: 'biography', component: BiographyComponent },
+      { path: 'companies', component: CompaniesComponent },
+      { path: 'partners', component: PartnersComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

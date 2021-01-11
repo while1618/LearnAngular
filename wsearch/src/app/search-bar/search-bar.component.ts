7e4592768d8d962730810f4259fa72b192f313d0
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-01-11 13:49:30 +0100

    wikipedia

--- wsearch/src/app/search-bar/search-bar.component.ts
+++ wsearch/src/app/search-bar/search-bar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent implements OnInit {
+  @Output() submitted = new EventEmitter<string>();
+  term = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onFormSubmit(event: Event) {
+    event.preventDefault();
+    this.submitted.emit(this.term);
   }
-
 }
--- wsearch/src/app/wikipedia.service.ts
+++ wsearch/src/app/wikipedia.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WikipediaService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  search(term: string) {
+    return this.httpClient.get('https://en.wikipedia.org/w/api.php', {
+      params: {
+        action: 'query',
+        format: 'json',
+        list: 'search',
+        utf8: '1',
+        srsearch: term,
+        origin: '*',
+      },
+    });
+  }
 }

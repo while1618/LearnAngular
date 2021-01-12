1176432441fded6b649da64db3954de608e5866b
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-01-12 14:39:33 +0100

    fetch photo complete

--- fetch-photos/src/app/photo-api.service.ts
+++ fetch-photos/src/app/photo-api.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { pluck } from 'rxjs/operators';
+
+interface PhotoApiResponse {
+  urls: {
+    regular: string;
+  };
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PhotoApiService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  fetch() {
+    return this.httpClient
+      .get<PhotoApiResponse>('https://api.unsplash.com/photos/random', {
+        headers: {
+          Authorization:
+            'Client-ID 08d19507529308e6b6874e4bce18a309019a26273da0556b967fa3969e972cf1',
+        },
+      })
+      .pipe(pluck('urls', 'regular'));
+  }
 }
--- fetch-photos/src/app/photo/photo.component.ts
+++ fetch-photos/src/app/photo/photo.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotoApiService } from '../photo-api.service';
 
 @Component({
   selector: 'app-photo',
   templateUrl: './photo.component.html',
-  styleUrls: ['./photo.component.css']
+  styleUrls: ['./photo.component.css'],
 })
 export class PhotoComponent implements OnInit {
+  image: string;
 
-  constructor() { }
+  constructor(private api: PhotoApiService) {}
 
   ngOnInit(): void {
+    this.fetchPhoto();
   }
 
+  onClick() {
+    this.fetchPhoto();
+  }
+
+  private fetchPhoto() {
+    this.api.fetch().subscribe((image) => {
+      this.image = image;
+    });
+  }
 }

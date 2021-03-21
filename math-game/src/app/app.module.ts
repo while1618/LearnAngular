1a0954cf96feff7ba202d3aca59fa7cbfeeb5ee5
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-03-21 23:32:49 +0100

    math game: custom directive with forms

--- math-game/src/app/app.module.ts
+++ math-game/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EquationComponent } from './equation/equation.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AnswerHighlightDirective } from './answer-highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent, EquationComponent],
+  declarations: [AppComponent, EquationComponent, AnswerHighlightDirective],
   imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],

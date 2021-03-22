f131639a7d9c3974d4614e4fe90025a88f9cd9a5
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-03-22 19:46:23 +0100

    email: reusable input

--- email/src/app/shared/input/input.component.ts
+++ email/src/app/shared/input/input.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent implements OnInit {
+  @Input() type: string;
+  @Input() label: string;
+  @Input() control: FormControl;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showErrors() {
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
   }
-
 }

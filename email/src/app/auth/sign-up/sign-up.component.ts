959fb134da4cb1d010a21fe8011cc9dfbb37e977
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-03-22 01:02:03 +0100

    email: signup/signin structure

---
+++ email/src/app/auth/sign-up/sign-up.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css'],
+})
+export class SignUpComponent implements OnInit {
+  signUpForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    passwordConfirmation: new FormControl(''),
+  });
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}

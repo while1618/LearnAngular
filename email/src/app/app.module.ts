ae6d71e5b07eaad6e8d4387d47bad83a8d7aa89a
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-03-23 20:18:03 +0100

    email: nav component

--- email/src/app/app.module.ts
+++ email/src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    HttpClientModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- email/src/app/shared/shared.module.ts
+++ email/src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { AuthRoutingModule } from '../auth/auth-routing.module';
 
 @NgModule({
-  declarations: [InputComponent],
-  imports: [CommonModule, ReactiveFormsModule],
-  exports: [InputComponent],
+  declarations: [InputComponent, NavMenuComponent],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
+  exports: [InputComponent, NavMenuComponent],
 })
 export class SharedModule {}

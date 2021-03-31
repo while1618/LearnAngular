9954b3f4d8b41187bfdf68082866bc016aa1a42e
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-03-31 21:01:27 +0200

    email: inbox init

---
+++ email/src/app/inbox/email.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+interface EmailsResponse {
+  id: string;
+  subject: string;
+  from: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmailService {
+  private static API_URL = 'https://api.angular-email.com';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getEmails() {
+    return this.httpClient.get<EmailsResponse[]>(
+      `${EmailService.API_URL}/emails`,
+      {
+        withCredentials: true,
+      }
+    );
+  }
+}
